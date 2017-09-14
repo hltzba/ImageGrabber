e879df2b8ec8334b4981b59d7e2601f526c733a8
Jimmy Wang <hltzba@163.com>
2017-09-15 00:38:44 +0800

    临时提交

--- ImageGrabber/Form1.Designer.cs
+++ ImageGrabber/Form1.Designer.cs
@@ -28,33 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colTitle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colUrl = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // listView1
+            // dataGridView1
             // 
-            this.listView1.Location = new System.Drawing.Point(21, 23);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(669, 371);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colTitle,
+            this.colUrl});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(920, 289);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // colTitle
+            // 
+            this.colTitle.HeaderText = "标题";
+            this.colTitle.Name = "colTitle";
+            this.colTitle.ReadOnly = true;
+            this.colTitle.Width = 54;
+            // 
+            // colUrl
+            // 
+            this.colUrl.HeaderText = "URL";
+            this.colUrl.Name = "colUrl";
+            this.colUrl.ReadOnly = true;
+            this.colUrl.Width = 48;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(268, 307);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(381, 372);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(702, 473);
-            this.Controls.Add(this.listView1);
+            this.ClientSize = new System.Drawing.Size(993, 720);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "图片爬虫";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTitle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colUrl;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- ImageGrabber/Form1.cs
+++ ImageGrabber/Form1.cs
@@ -42,8 +42,53 @@ namespace ImageGrabber
                 foreach (var item in nodes)
                 {
                     string href = item.GetAttributeValue("href", "");
-                    listView1.Items.Add("http://c.n.h.k.xbluntan.space/" + href);
+                    string title = item.InnerText;
+                    if (!title.Contains("P】"))
+                    {
+                        continue;
+                    }
+                    int i = dataGridView1.Rows.Add();
+                    var row = dataGridView1.Rows[i];
+
+                    row.Cells["colTitle"].Value = title;
+                    row.Cells["colUrl"].Value = "http://c.n.h.k.xbluntan.space/" + href;
+                    // listView1.Items.Add("http://c.n.h.k.xbluntan.space/" + href);
                     // textBox1.AppendText("http://c.n.h.k.xbluntan.space/" + href + "\r\n");
+                }
+            }
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string url = dataGridView1.Rows[e.RowIndex].Cells["colUrl"].Value.ToString();
+            HttpClient NetworkClient = new HttpClient();
+            var ctx = NetworkClient.Create<string>(HttpMethod.Get, url).Send();
+            if (!ctx.IsValid())
+            {
+                MessageBox.Show("无效获取");
+                return;
+            }
+
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(ctx.Result);
+            var nodes = doc.DocumentNode.SelectNodes("//*[contains(@id,'aimg_')]");
+            if (nodes != null && nodes.Count > 0)
+            {
+                var imgsrc = nodes[0].GetAttributeValue("file", "");
+                if (string.IsNullOrEmpty(imgsrc))
+                {
+                    MessageBox.Show("未获取到图片");
+                }
+                else
+                {
+                    HttpClient ImgClient = new HttpClient();
+                    var img = NetworkClient.Create<Image>(HttpMethod.Get, imgsrc).Send();
+                    if (!ctx.IsValid())
+                    {
+                        MessageBox.Show("无效获取");
+                        return;
+                    }
+                    pictureBox1.Image = img.Result;
                 }
             }
         }
